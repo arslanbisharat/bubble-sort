1398f0d05c58ff0180c223d5311a6b05dfb1bfc5
Muhammad Arslan <58504655+arslanbisharat@users.noreply.github.com>
2020-04-03 00:51:16 +0500

    Create bubble_sort-by.rb

---
+++ bubble_sort-by.rb
@@ -0,0 +1,17 @@
+def bubble_sort_by(array)
+    return array if array.size <= 1
+    swap = true
+      while swap
+        swap = false
+        (array.length - 1).times do |x|
+          if (yield(array[x], array[x+1]) > 0)
+            array[x], array[x+1] = array[x+1], array[x]
+            swap = true
+          end
+        end
+      end
+    array
+end
+  p bubble_sort_by(['hi', 'hello', 'hey']) { |left, right|
+    left.length - right.length
+    }
